751665c8d7194099a0086dda1fef4cd151184842
DiogoPCS <diogo.paulino.santos@hotmail.com>
2024-08-09 14:49:09 -0300

    variavel pessoa criada

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,5 +11,17 @@ import { MessageService } from '../services/message.service';
 })
 export class HomePage {
 
+  pessoa = {
+    foto: 'https://professionalmoron.com/wp-content/uploads/2012/05/alpaca-985158_640.jpg',
+    nome: 'Barrigudinha Seleide',
+    objetivo: 'Programador HTML & CSS',
+    contato: {
+      telefone: '1596666666',
+      email: 'seleide@hotmail.com',
+      github: 'github.com/seleide',
+      linkedin: 'linkedin.com/barrigudinha'
+    }
+  }
+
   constructor(){ }
 }
