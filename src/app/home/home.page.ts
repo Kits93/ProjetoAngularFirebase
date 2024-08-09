83eb65b93379e405557cb40e7e4cff4e42c6a1fa
DiogoPCS <diogo.paulino.santos@hotmail.com>
2024-08-09 13:33:56 -0300

    teste

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -11,5 +11,49 @@ import { MessageService } from '../services/message.service';
 })
 export class HomePage {
 
-  constructor(){ }
+  pessoa = {
+    foto: `https://i.pinimg.com/736x/29/66/d1/2966d11abca07e3cb869b1f9700ad71d.jpg`,
+    nome: `Barrigudinha Seleide`,
+    objetivo: `Programador HTML & CSS`,
+    contato: {
+      telefone: '966666666',
+      email: 'seleide@hotmail.com',
+      github: 'github.com/seleide',
+      linkedin: 'linkedin.com/seleide'
+    },
+    softskills: [
+      'Comunicação',
+      'Trabalho em Equipe',
+      'Proatividade',
+      'Criatividade',
+      'Inovação'
+    ],
+    formacao_academica: [
+      {
+        ano_inicio: '2023',
+        ano_fim: '2025',
+        instituicao: 'Etec Sales Gomes',
+        curso: 'Desenvolvimento de Sistemas'
+      },
+      {
+        ano_inicio: '2025',
+        ano_fim: '2028',
+        instituicao: 'Faculdade de Tencologia de Tatui - Fatec',
+        curso: 'Análise e Desenvolvimento de Sistemas'
+      }
+    ]
+  }
+
+
+  constructor(){
+    this.teste();
+  }
+
+  teste(){
+    console.log(this.pessoa);
+  }
+
+  
+
+  
 }
