96fca0033b70d4d7eb459a736aee8a89b4ca5691
DiogoPCS <diogo.paulino.santos@hotmail.com>
2024-11-07 19:51:05 -0300

    t3ste

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: 'menu.menu.html',
+  styleUrls: ['menu.menu.scss'],
+})
+export class MenuComponent {
+
+  constructor( ){ }
+
+}
